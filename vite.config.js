9e52a17e47fb27f7ac86c9eec0a48c138df75e0a
Aram Rafeq <aramrafeq2@gmail.com>
2022-12-04 13:18:08 +0300

    base url added to build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/game-of-life/', // for sake of github pages
   build: {
     outDir: 'docs' // for sake of github pages 
   }
